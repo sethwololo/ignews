40329bdb1b7d786479064dfad6bd05d6b66e9487
Matheus Henrique <54484095+sethwololo@users.noreply.github.com>
2021-04-26 12:05:47 -0300

    Componente: Subscribe Button

---
+++ src/components/SubscribeButton/index.tsx
@@ -0,0 +1,12 @@
+import styles from './styles.module.scss';
+
+export function SubscribeButtton() {
+  return (
+    <button
+      type="button"
+      className={styles.subscribeButton}
+    >
+      Subscribe now
+    </button>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import { SubscribeButtton } from '../components/SubscribeButton';
 
 import styles from './home.module.scss';
 
@@ -17,6 +18,7 @@ export default function Home() {
           Get access to all the publications <br/>
           <span>for $ 9.90/month</span>
         </p>
+        <SubscribeButtton />
       </section>
       <img src="/images/avatar.svg" alt="Girl Coding"/>
     </main>
