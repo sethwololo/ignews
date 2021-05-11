546584fae3f736bcc2c197e30bf45ed52b4c6ff1
Matheus Henrique <sethwololo@gmail.com>
2021-05-11 11:48:27 -0300

    Ouvindo webhooks

--- src/pages/api/_lib/manageSubscription.ts
+++ src/pages/api/_lib/manageSubscription.ts
@@ -2,10 +2,14 @@ import { query as q } from 'faunadb';
 import { fauna } from "../../../services/fauna";
 import { stripe } from '../../../services/stripe';
 
-export async function saveSubscription(subscriptionId: string, customerId: string) {
+export async function saveSubscription(
+  subscriptionId: string,
+  customerId: string,
+  createAction = false,
+) {
   const userRef = await fauna.query(
     q.Select(
-      "ref",
+      'ref',
       q.Get(
         q.Match(
           q.Index('user_by_stripe_customer_id'),
@@ -24,10 +28,29 @@ export async function saveSubscription(subscriptionId: string, customerId: strin
     price_id: subscription.items.data[0].price.id,
   };
 
-  await fauna.query(
-    q.Create(
-      q.Collection('subscriptions'),
-      { data: subscriptionData }
-    )
-  );
+  console.log(subscriptionData);
+
+  if (createAction) {
+    await fauna.query(
+      q.Create(
+        q.Collection('subscriptions'),
+        { data: subscriptionData }
+      )
+    );
+  } else {
+    await fauna.query(
+      q.Replace(
+        q.Select(
+          'ref',
+          q.Get(
+            q.Match(
+              q.Index('subscription_by_index'),
+              subscription.id,
+            )
+          )
+        ),
+        { data: subscriptionData }
+      )
+    );
+  }
 }
--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -23,20 +23,22 @@ export const config = {
 }
 
 const relevantEvents = new Set([
-  'checkout.session.completed'
+  'checkout.session.completed',
+  'customer.subscription.updated',
+  'customer.subscription.deleted',
 ]);
 
-export default async (req: NextApiRequest, res: NextApiResponse) => {
-  if (req.method === 'POST') {
-    const buf = await buffer(req);
-    const secret = req.headers['stripe-signature'];
+export default async (request: NextApiRequest, response: NextApiResponse) => {
+  if (request.method === 'POST') {
+    const buf = await buffer(request);
+    const secret = request.headers['stripe-signature'];
 
     let event: Stripe.Event;
 
     try {
       event = stripe.webhooks.constructEvent(buf, secret, process.env.STRIPE_WEBHOOK_SECRET);
     } catch (err) {
-      return res.status(400).send(`Webhook error: ${err.message}`);
+      return response.status(400).send(`Webhook error: ${err.message}`);
     }
 
     const { type } = event;
@@ -44,27 +46,38 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     if (relevantEvents.has(type)) {
       try {
         switch (type) {
+          case 'customer.subscription.updated':
+          case 'customer.subscription.deleted':
+
+            const subscription = event.data.object as Stripe.Subscription;
+            await saveSubscription(
+              subscription.id,
+              subscription.customer.toString(),
+              false
+            );
+
+            break;
           case 'checkout.session.completed':
 
             const checkoutSession = event.data.object as Stripe.Checkout.Session;
-
             await saveSubscription(
               checkoutSession.subscription.toString(),
               checkoutSession.customer.toString(),
-            )
+              true
+            );
 
             break;
           default:
-            throw new Error('Unhandled event.')
+            throw new Error('Unhandled event.');
         }
-      } catch (error) {
-        return res.json({error: 'Webhook handler failed.'})
+      } catch (err) {
+        return response.json({ error: 'Webhook handler failed' });
       }
     }
 
-    res.json({ received: true });
+    response.json({ received: true });
   } else {
-    res.setHeader('Allow', 'POST')
-    res.status(405).end('Method not allowed')
+    response.setHeader('Allow', 'POST');
+    response.status(405).send('Method not allowed');
   }
 }
