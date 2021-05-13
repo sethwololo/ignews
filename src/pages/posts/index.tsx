65ee83639a48047b380b1c32c1f61945cb061890
Matheus Henrique <sethwololo@gmail.com>
2021-05-13 17:51:21 -0300

    Página: Post

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -6,6 +6,7 @@ import { RichText } from 'prismic-dom';
 import { getPrismicClient } from '../../services/prismic';
 
 import styles from './styles.module.scss';
+import Link from 'next/link';
 
 type Post = {
   slug: string;
@@ -29,13 +30,15 @@ export default function Posts({ posts }: PostsProps) {
         <div className={styles.posts}>
 
           {posts.map(post => (
-            <a key={post.slug}>
-              <time>
-                {post.updatedAt}
-              </time>
-              <strong>{post.title}</strong>
-              <p>{post.excerpt}</p>
-            </a>
+            <Link key={post.slug} href={`/posts/${post.slug}`}>
+              <a>
+                <time>
+                  {post.updatedAt}
+                </time>
+                <strong>{post.title}</strong>
+                <p>{post.excerpt}</p>
+              </a>
+            </Link>
           ))}
         </div>
       </main>
